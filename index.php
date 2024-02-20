ab9fd959378f3a419fdc1159ef9e168f9adabca8
Stephen Yeargin <stephen.yeargin@gmail.com>
2024-02-19 21:54:51 -0600

    Fix color of success message, update library

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ if ($_POST) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Support Our Campaign</title>
 <meta name="description" content="Securely contribute to our campaign.">
-<script src="//js.stripe.com/v2/"></script>
+<script src="https:///js.stripe.com/v2/" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -288,7 +288,7 @@ $(function() {
 
         <?php elseif ($success): ?>
         <div class="card border-success mb-3">
-          <div class="card-header text-bg-danger h4">Thank you for your support!</div>
+          <div class="card-header text-bg-success h4">Thank you for your support!</div>
           <div class="card-body">
             <p>We're processing your contribution to our campaign. Thank you for your support!</p>
             <p><a href="/" class="btn btn-primary">Back to Website</a></p>
